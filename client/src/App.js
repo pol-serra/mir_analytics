5f11f3cf3177732ede2bfc9bd8fe169a7157f823
Pol Serra Montes <pol.serra.i.montes@estudiantat.upc.edu>
2023-10-29 12:04:21 +0100

    without using

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,11 @@
 import React, { useState, useEffect } from 'react';
 
 function App() {
-  const [data, setData] = useState('');
-
-  useEffect(() => {
-    fetch('/api') // Ruta del servidor Flask
-      .then(response => response.text())
-      .then(data => setData(data));
-  }, []);
 
   return (
     <div className="App">
       <h1>React with Flask</h1>
-      <p>Response from Flask: {data}</p>
+      <p>Response from Flask:</p>
     </div>
   );
 }
